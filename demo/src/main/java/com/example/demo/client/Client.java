2869bc4b2224a10e7a4ffa16f70024d26e322112
abderrahmen018 <133798954+abderrahmen018@users.noreply.github.com>
2024-04-22 15:07:40 +0100

    Add files via upload
    @chemsou rigllna switch fonction

--- demo/src/main/java/com/example/demo/client/Client.java
+++ demo/src/main/java/com/example/demo/client/Client.java
@@ -6,13 +6,27 @@ public class Client {
     private int id;
     private String fullname;
     private String email;
+    private String password;
     private String  phone;
 
-    public Client(int id, String fullname, String email, String phone) {
+    public Client(int id, String fullname,String password, String email, String phone) {
         this.id = id;
         this.fullname = fullname;
         this.email = email;
+        this.password = password;
         this.phone = phone;
     }
+
+    public String getFullname() { return fullname; }
+    public void setFullname(String fullname) { this.fullname = fullname; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
 
 }
