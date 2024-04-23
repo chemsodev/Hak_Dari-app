71bf2ec62d3e92663111c81891755f399f798e7f
chemso <bourabiachemseddine@gmail.com>
2024-04-23 22:26:28 +0200

    removed connecting to database issue

--- demo/src/main/java/com/example/demo/Database.java
+++ demo/src/main/java/com/example/demo/Database.java
@@ -13,12 +13,8 @@ public class Database {
 
         try {
 
-            Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Connecting to database...");
             return DriverManager.getConnection("jdbc:mysql://mysql-11303ae7-poodb.a.aivencloud.com:11410/dbusers", "avnadmin", "AVNS_yEC1BwhAN6l7KmBlD8n");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Could not load MySQL driver");
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Could not connect to database");
