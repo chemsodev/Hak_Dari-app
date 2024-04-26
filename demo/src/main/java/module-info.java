332e1a4ba2e012861bc174cd6941052dd747eb8a
chems eddine bourabia <bourabiachemseddine@gmail.COM>
2024-04-26 12:49:16 +0100

    Delete demo directory

--- demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
-}
